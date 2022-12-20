5bf2e6de1b61038dc29a3d479d5d1d81fcc96e50
Benyamin Galeano <benyamin.galeano@gmail.com>
2022-12-19 22:32:04 -0600

    feat: 🎸 install script for launch

---
+++ install.ts
@@ -0,0 +1,23 @@
+import { brightGreen } from "https://deno.land/std@0.168.0/fmt/colors.ts"
+
+const process = Deno.run({
+  cmd: [
+    Deno.execPath(),
+    "install",
+    "--import-map=import_map.json",
+    "-Af",
+    "--name=launch",
+    import.meta.resolve("./cli/main.ts")
+  ]
+})
+
+const status = await process.status()
+
+process.close()
+
+if (!status.success) {
+  console.log("Failed to install launch")
+  Deno.exit(status.code)
+}
+
+console.log(brightGreen("ok!"))
